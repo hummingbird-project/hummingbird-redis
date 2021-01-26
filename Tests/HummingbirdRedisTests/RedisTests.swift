4d5937cc3962b5136f8f1200667b3af3e86fe5db
Adam Fowler <adamfowler71@gmail.com>
2021-01-26 14:24:42 +0000

    Update redis tests to include route test

--- Tests/HummingbirdRedisTests/RedisTests.swift
+++ Tests/HummingbirdRedisTests/RedisTests.swift
@@ -1,9 +1,10 @@
 import Hummingbird
+import HummingbirdXCT
 @testable import HummingbirdRedis
 import XCTest
 
 final class HummingbirdRedisTests: XCTestCase {
-    func testApplicationRedis() throws {
+    func testApplication() throws {
         let app = HBApplication()
         try app.addRedis(configuration: .init(hostname: "localhost", port: 6379))
 
@@ -11,24 +12,20 @@ final class HummingbirdRedisTests: XCTestCase {
         XCTAssertEqual(info.string?.contains("redis_version"), true)
     }
 
-    func testRouteHandlerRedis() throws {
-/*        let app = HBApplication(configuration: .init(address: .hostname(port: Int.random(in: 4000..<9000))))
-        try app.redis.initialize(configuration: .init(hostname: "localhost", port: 6379))
+    func testRouteHandler() throws {
+        let app = HBApplication(testing: .live)
+        try app.addRedis(configuration: .init(hostname: "localhost", port: 6379))
         app.router.get("redis") { req in
             req.redis.send(command: "INFO").map {
                 $0.description
             }
         }
-        app.start()
-        defer { app.stop(); app.wait() }
+        app.XCTStart()
+        defer { app.stop(); }
 
-        let client = HTTPClient(eventLoopGroupProvider: .shared(app.eventLoopGroup))
-        defer { XCTAssertNoThrow(try client.syncShutdown()) }
-
-        let future = client.get(url: "http://localhost:\(app.configuration.address.port!)/redis").flatMapThrowing { response in
+        app.XCTExecute(uri: "/redis", method: .GET) { response in
             var body = try XCTUnwrap(response.body)
             XCTAssertEqual(body.readString(length: body.readableBytes)?.contains("redis_version"), true)
         }
-        XCTAssertNoThrow(try future.wait())*/
     }
 }
