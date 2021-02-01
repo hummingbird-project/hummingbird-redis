c45368185143099d66f9b44df27a142578b6e555
Adam Fowler <adamfowler71@gmail.com>
2021-02-01 17:43:37 +0000

    Use hummingbird v0.1.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "HummingbirdRedis", targets: ["HummingbirdRedis"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adam-fowler/hummingbird.git", .branch("main")),
+        .package(url: "https://github.com/adam-fowler/hummingbird.git", from: "0.1.0"),
         .package(url: "https://gitlab.com/mordil/RediStack.git", from: "1.1.0"),
     ],
     targets: [
