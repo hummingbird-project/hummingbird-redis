0e509c192586fea446a744a83134f774acd1cdf7
Adam Fowler <adamfowler71@gmail.com>
2022-11-22 14:06:12 +0000

    Move redis connection pool group code out of Application

--- Sources/HummingbirdRedis/Application+Redis.swift
+++ Sources/HummingbirdRedis/Application+Redis.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Hummingbird server framework project
 //
-// Copyright (c) 2021-2021 the Hummingbird authors
+// Copyright (c) 2021-2022 the Hummingbird authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -13,119 +13,20 @@
 //===----------------------------------------------------------------------===//
 
 import Hummingbird
-import Lifecycle
-import LifecycleNIOCompat
-import RediStack
-
 extension HBApplication {
-    /// Redis interface
-    public struct Redis {
-        init(configuration: RedisConfiguration, application: HBApplication) {
-            self.configuration = configuration
-            self.application = application
-            Self.createPools(configuration: configuration, application: application)
-            self.pubsubClient = application.eventLoopStorage.first { _ in true }!.storage.redisPool
-
-            application.lifecycle.registerShutdown(label: "Redis", .eventLoopFuture(self.closePools))
-        }
-
-        public func pool(for eventLoop: EventLoop) -> RedisConnectionPool {
-            return self.application.eventLoopStorage.get(for: eventLoop).redisPool
-        }
-
-        private static func createPools(configuration: RedisConfiguration, application: HBApplication) {
-            application.eventLoopStorage.forEach { eventLoop, storage in
-                storage.redisPool = RedisConnectionPool(
-                    configuration: .init(configuration, defaultLogger: application.logger),
-                    boundEventLoop: eventLoop
-                )
-            }
-        }
-
-        private func closePools() -> EventLoopFuture<Void> {
-            let poolCloseFutures: [EventLoopFuture<Void>] = self.application.eventLoopStorage.map { ev, storage in
-                let promise = ev.makePromise(of: Void.self)
-                storage.redisPool.close(promise: promise)
-                return promise.futureResult
-            }
-            return EventLoopFuture.andAllComplete(poolCloseFutures, on: self.application.eventLoopGroup.next())
-        }
-
-        var pubsubClient: RedisClient
-
-        private unowned var application: HBApplication
-        private var configuration: RedisConfiguration
-    }
-
-    public var redis: Redis {
+    public var redis: RedisConnectionPoolGroup {
         self.extensions.get(\.redis)
     }
 
     /// Add Redis to HBApplication
     /// - Parameter configuration: Redis configuration
-    public func addRedis(configuration: RedisConfiguration) {
-        self.extensions.set(\.redis, value: .init(configuration: configuration, application: self))
-    }
-}
-
-extension HBApplication.EventLoopStorage {
-    var redisPool: RedisConnectionPool {
-        get { extensions.get(\.redisPool) }
-        set { extensions.set(\.redisPool, value: newValue) }
-    }
-}
-
-extension HBApplication.Redis: RedisClient {
-    public var eventLoop: EventLoop {
-        self.application.eventLoopGroup.next()
-    }
-
-    public func logging(to logger: Logger) -> RedisClient {
-        self.pool(for: self.eventLoop)
-            .logging(to: logger)
-    }
-
-    public func send(command: String, with arguments: [RESPValue]) -> EventLoopFuture<RESPValue> {
-        self.pool(for: self.eventLoop)
-            .logging(to: self.application.logger)
-            .send(command: command, with: arguments)
-    }
-
-    public func subscribe(
-        to channels: [RedisChannelName],
-        messageReceiver receiver: @escaping RedisSubscriptionMessageReceiver,
-        onSubscribe subscribeHandler: RedisSubscriptionChangeHandler?,
-        onUnsubscribe unsubscribeHandler: RedisSubscriptionChangeHandler?
-    ) -> EventLoopFuture<Void> {
-        return self
-            .pubsubClient
-            .logging(to: self.application.logger)
-            .subscribe(to: channels, messageReceiver: receiver, onSubscribe: subscribeHandler, onUnsubscribe: unsubscribeHandler)
-    }
-
-    public func unsubscribe(from channels: [RedisChannelName]) -> EventLoopFuture<Void> {
-        return self
-            .pubsubClient
-            .logging(to: self.application.logger)
-            .unsubscribe(from: channels)
-    }
-
-    public func psubscribe(
-        to patterns: [String],
-        messageReceiver receiver: @escaping RedisSubscriptionMessageReceiver,
-        onSubscribe subscribeHandler: RedisSubscriptionChangeHandler?,
-        onUnsubscribe unsubscribeHandler: RedisSubscriptionChangeHandler?
-    ) -> EventLoopFuture<Void> {
-        return self
-            .pubsubClient
-            .logging(to: self.application.logger)
-            .psubscribe(to: patterns, messageReceiver: receiver, onSubscribe: subscribeHandler, onUnsubscribe: unsubscribeHandler)
-    }
-
-    public func punsubscribe(from patterns: [String]) -> EventLoopFuture<Void> {
-        return self
-            .pubsubClient
-            .logging(to: self.application.logger)
-            .punsubscribe(from: patterns)
+    public func addRedis(configuration: HBRedisConfiguration) {
+        self.extensions.set(\.redis, value: .init(
+            configuration: configuration,
+            eventLoopGroup: self.eventLoopGroup,
+            logger: self.logger
+        )) { redis in
+            try redis.closePools().wait()
+        }
     }
 }
---
+++ Sources/HummingbirdRedis/RedisConnectionPoolGroup.swift
@@ -0,0 +1,126 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2022 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Hummingbird
+import Lifecycle
+import LifecycleNIOCompat
+import RediStack
+
+/// Store Redis connection pool array for an EventLoopGroup.
+///
+/// Provides a connection pool for each EventLoop in the EventLoopGroup
+public struct RedisConnectionPoolGroup {
+    /// Initialise RedisConnectionPoolGroup
+    /// - Parameters:
+    ///   - configuration: redis configuration
+    ///   - eventLoopGroup: EventLoopGroup to create connections for
+    ///   - logger: Logger
+    public init(configuration: HBRedisConfiguration, eventLoopGroup: EventLoopGroup, logger: Logger) {
+        var pools: [EventLoop.RedisKey: RedisConnectionPool] = [:]
+        for eventLoop in eventLoopGroup.makeIterator() {
+            pools[eventLoop.redisKey] = RedisConnectionPool(
+                configuration: .init(configuration, defaultLogger: logger),
+                boundEventLoop: eventLoop
+            )
+        }
+        self.pools = pools
+        self.eventLoopGroup = eventLoopGroup
+    }
+
+    /// Get connection pool for EventLoop
+    /// - Parameter eventLoop: eventLoop
+    /// - Returns: connection pool
+    public func pool(for eventLoop: EventLoop) -> RedisConnectionPool {
+        guard let pool = self.pools[eventLoop.redisKey] else {
+            preconditionFailure("EventLoop must be from Application EventLoopGroup")
+        }
+        return pool
+    }
+
+    /// Close all connection pools
+    /// - Returns: EventLoopFuture for when it is done
+    func closePools() -> EventLoopFuture<Void> {
+        let poolCloseFutures: [EventLoopFuture<Void>] = self.pools.values.map { pool in
+            let promise = pool.eventLoop.makePromise(of: Void.self)
+            pool.close(promise: promise)
+            return promise.futureResult
+        }
+        return EventLoopFuture.andAllComplete(poolCloseFutures, on: self.eventLoopGroup.next())
+    }
+
+    let pools: [EventLoop.RedisKey: RedisConnectionPool]
+    let eventLoopGroup: EventLoopGroup
+}
+
+extension EventLoop {
+    typealias RedisKey = ObjectIdentifier
+    var redisKey: RedisKey {
+        ObjectIdentifier(self)
+    }
+}
+
+/// Extend `RedisConnectionPoolGroup`` to provide `RedisClient`` functionality
+extension RedisConnectionPoolGroup: RedisClient {
+    public var eventLoop: EventLoop {
+        self.eventLoopGroup.next()
+    }
+
+    public var pubsubClient: RedisClient {
+        self.pools.first!.value
+    }
+
+    public func logging(to logger: Logger) -> RedisClient {
+        self.pool(for: self.eventLoop)
+            .logging(to: logger)
+    }
+
+    public func send(command: String, with arguments: [RESPValue]) -> EventLoopFuture<RESPValue> {
+        self.pool(for: self.eventLoop)
+            .send(command: command, with: arguments)
+    }
+
+    public func subscribe(
+        to channels: [RedisChannelName],
+        messageReceiver receiver: @escaping RedisSubscriptionMessageReceiver,
+        onSubscribe subscribeHandler: RedisSubscriptionChangeHandler?,
+        onUnsubscribe unsubscribeHandler: RedisSubscriptionChangeHandler?
+    ) -> EventLoopFuture<Void> {
+        return self
+            .pubsubClient
+            .subscribe(to: channels, messageReceiver: receiver, onSubscribe: subscribeHandler, onUnsubscribe: unsubscribeHandler)
+    }
+
+    public func unsubscribe(from channels: [RedisChannelName]) -> EventLoopFuture<Void> {
+        return self
+            .pubsubClient
+            .unsubscribe(from: channels)
+    }
+
+    public func psubscribe(
+        to patterns: [String],
+        messageReceiver receiver: @escaping RedisSubscriptionMessageReceiver,
+        onSubscribe subscribeHandler: RedisSubscriptionChangeHandler?,
+        onUnsubscribe unsubscribeHandler: RedisSubscriptionChangeHandler?
+    ) -> EventLoopFuture<Void> {
+        return self
+            .pubsubClient
+            .psubscribe(to: patterns, messageReceiver: receiver, onSubscribe: subscribeHandler, onUnsubscribe: unsubscribeHandler)
+    }
+
+    public func punsubscribe(from patterns: [String]) -> EventLoopFuture<Void> {
+        return self
+            .pubsubClient
+            .punsubscribe(from: patterns)
+    }
+}
