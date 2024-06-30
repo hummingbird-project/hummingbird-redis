5b649248e5bba3d5382f28fffbfcc02d00e46ec9
Adam Fowler <adamfowler71@gmail.com>
2024-06-30 09:29:35 +0100

    Remove Jobs as it is in its own package now

--- Package.swift
+++ Package.swift
@@ -8,11 +8,9 @@ let package = Package(
     platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
     products: [
         .library(name: "HummingbirdRedis", targets: ["HummingbirdRedis"]),
-        .library(name: "HummingbirdJobsRedis", targets: ["HummingbirdJobsRedis"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", branch: "main"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-jobs.git", branch: "main"),
         .package(url: "https://github.com/swift-server/RediStack.git", from: "1.4.0"),
     ],
     targets: [
@@ -20,21 +18,9 @@ let package = Package(
             .product(name: "Hummingbird", package: "hummingbird"),
             .product(name: "RediStack", package: "RediStack"),
         ]),
-        .target(name: "HummingbirdJobsRedis", dependencies: [
-            .byName(name: "HummingbirdRedis"),
-            .product(name: "Hummingbird", package: "hummingbird"),
-            .product(name: "HummingbirdJobs", package: "hummingbird-jobs"),
-            .product(name: "RediStack", package: "RediStack"),
-        ]),
         .testTarget(name: "HummingbirdRedisTests", dependencies: [
             .byName(name: "HummingbirdRedis"),
             .product(name: "Hummingbird", package: "hummingbird"),
-            .product(name: "HummingbirdTesting", package: "hummingbird"),
-        ]),
-        .testTarget(name: "HummingbirdJobsRedisTests", dependencies: [
-            .byName(name: "HummingbirdJobsRedis"),
-            .product(name: "Hummingbird", package: "hummingbird"),
-            .product(name: "HummingbirdJobs", package: "hummingbird-jobs"),
             .product(name: "HummingbirdTesting", package: "hummingbird"),
         ]),
     ]
